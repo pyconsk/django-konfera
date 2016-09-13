adb20ec4f68436fb30e672efad8697fd2b26d650
16961714b <16961714b@gmail.com>
2016-09-13 20:46:46 +0200

    add Event model

--- konfera/models.py
+++ konfera/models.py
@@ -24,3 +24,23 @@ class Location(models.Model):
             self.title, self.street, self.street2, self.city,
             self.postcode, self.state, self.capacity)
 
+
+EVENT_TYPE_CHOICES = (
+    ('conference', 'Conference'),
+    ('meetup', 'Meetup'),
+)
+
+EVENT_STATUS_CHOICES = (
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+    ('expired', 'Expired'),
+)
+
+class Event(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    date_from = models.DateTimeField()
+    date_to = models.DateTimeField()
+    event_type = models.CharField(choices=EVENT_TYPE_CHOICES, max_length=20)
+    status = models.CharField(choices=EVENT_STATUS_CHOICES, max_length=20)
+    location = models.ForeignKey('Location')
