ddf7640063cd51615aa0d96d3595240e6208edb2
Tomáš Horváth <tomas.horvath@gmail.com>
2016-09-13 20:22:44 +0200

    receipt model

--- konfera/models.py
+++ konfera/models.py
@@ -16,3 +16,19 @@ class Location(models.Model):
     def __str__(self):
         return "Location: %s %s %s %s %s %s %s" % self.title, self.street, self.street2, self.city, self.postcode, self.state, self.capacity
 
+
+class Receipt(models.Model):
+
+    title = models.CharField(max_length=128)
+    street = models.CharField(max_length=128)
+    street2 = models.CharField(max_length=128)
+    city = models.CharField(max_length=128)
+    postcode = models.CharField(max_length=12)
+    state = models.CharField(max_length=128)
+    companyid = models.CharField(max_length=32)
+    taxid = models.CharField(max_length=32)
+    vatid = models.CharField(max_length=32)
+    amount = models.FloatField()
+
+    def __str__(self):
+        return self.title
