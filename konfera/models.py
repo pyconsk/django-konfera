8de0e25f62c8117537542ca6bf44bb81270245f9
Andy <andrejbalaz001@gmail.com>
2016-09-13 20:33:28 +0200

    Resolve conflicts

--- konfera/models.py
+++ konfera/models.py
@@ -7,3 +7,20 @@ class Room(models.Model):
     
     def __str__(self):
         return self.title
+
+
+class Location(models.Model):
+
+    title = models.CharField(max_length=128)
+    street = models.CharField(max_length=128)
+    street2 = models.CharField(max_length=128)
+    city = models.CharField(max_length=128)
+    postcode = models.CharField(max_length=12)
+    state = models.CharField(max_length=128)
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return "Location: {} {} {} {} {} {} {}".format(
+            self.title, self.street, self.street2, self.city,
+            self.postcode, self.state, self.capacity)
+
