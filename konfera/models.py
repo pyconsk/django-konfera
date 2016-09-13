25c708b4e535ae37a216135ddbfea71e7510d642
Tomáš Horváth <tomas.horvath@gmail.com>
2016-09-13 21:06:20 +0200

    Discount Codes model

--- konfera/models.py
+++ konfera/models.py
@@ -70,4 +70,20 @@ class TicketType(models.Model):
     price = models.DecimalField()
     available_from = models.DateTimeField()
     available_to = models.DateTimeField()
-    event = models.ForeignKey(Event, null=False)
+    event = models.ForeignKey('Event', null=False)
+
+    def __str__(self):
+        return self.title
+
+
+class DiscountCodes(models.Model):
+    title = models.CharField(max_length=128)
+    hash = models.CharField(max_length=64)
+    discount = models.IntegerField(max(100), min(0))
+    available_from = models.DateTimeField()
+    available_to = models.DateTimeField()
+    usage = models.IntegerField()
+    ticket_type = models.ForeignKey('TicketType', null=False)
+
+    def __str__(self):
+        return self.title
