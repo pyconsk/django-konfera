73b97be1e6936bbfee6c711f3337b614f99e8671
Roman Beňo <romanko.sk@gmail.com>
2016-09-13 20:10:48 +0200

    Applied all the PEP8 rules

--- konfera/models.py
+++ konfera/models.py
@@ -4,15 +4,17 @@ from django.db import models
 
 
 class Location(models.Model):
-    
-    title = models.CharField(max_length = 128)
-    street = models.CharField(max_length = 128)
-    street2 = models.CharField(max_length = 128)
-    city = models.CharField(max_length = 128)
-    postcode = models.CharField(max_length = 12)
-    state = models.CharField(max_length = 128)
+
+    title = models.CharField(max_length=128)
+    street = models.CharField(max_length=128)
+    street2 = models.CharField(max_length=128)
+    city = models.CharField(max_length=128)
+    postcode = models.CharField(max_length=12)
+    state = models.CharField(max_length=128)
     capacity = models.IntegerField()
 
     def __str__(self):
-        return "Location: %s %s %s %s %s %s %s" % self.title, self.street, self.street2, self.city, self.postcode, self.state, self.capacity
+        return "Location: {} {} {} {} {} {} {}".format(
+            self.title, self.street, self.street2, self.city,
+            self.postcode, self.state, self.capacity)
 
