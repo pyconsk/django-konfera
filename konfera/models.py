5e6e4cda07586c47e3d6b7ade12a74896613267c
Andy <andrejbalaz001@gmail.com>
2016-09-13 20:09:23 +0200

    Add model Room

--- konfera/models.py
+++ konfera/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Room(models.Model):
+    title = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.title
