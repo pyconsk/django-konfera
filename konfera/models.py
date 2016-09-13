1c0f96b12a796eb2d3d45b9e0cb20914d11e7071
jbezrucka <juraj.bezrucka@gmail.com>
2016-09-13 20:33:53 +0200

    receipt model

--- konfera/models.py
+++ konfera/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Receipt(models.Model):
+    title = models.CharField(max_length=128)
+    street = models.CharField(max_length=128)
+    street2 = models.CharField(max_length=128)
+    city = models.CharField(max_length=128)
+    postcode = models.CharField(max_length=12)
+    state = models.CharField(max_length=128)
+    companyid = models.CharField(max_length=32)
+    taxid = models.CharField(max_length=32)
+    vatid = models.CharField(max_length=32)
+    amount = models.FloatField()
+
