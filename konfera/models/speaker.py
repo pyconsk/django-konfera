3a42915f9ffe262016a00aa95b1471a86be8a87d
Zoltan Onody <zoltan.onody@gmail.com>
2016-09-16 00:15:42 +0200

    Add title to Speaker model

--- konfera/models/speaker.py
+++ konfera/models/speaker.py
@@ -1,9 +1,23 @@
 from django.db import models
 
 
+TITLE_UNSET = 'none'
+
+TITLE_CHOICES = (
+    (TITLE_UNSET, ''),
+    ('mr', 'Mr.'),
+    ('ms', 'Ms.'),
+)
+
+
 class Speaker(models.Model):
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
+    title = models.CharField(
+        choices=TITLE_CHOICES,
+        max_length=4,
+        default=TITLE_UNSET
+    )
     email = models.EmailField(max_length=255)
     phone = models.CharField(max_length=64)
     bio = models.TextField()
@@ -13,4 +27,8 @@ class Speaker(models.Model):
     sponsor = models.ForeignKey('Sponsor', default=None)
 
     def __str__(self):
-        return self.title
+        return '{title} {first_name} {last_name}'.format(
+            title=dict(TITLE_CHOICES)[self.title],
+            first_name=self.first_name,
+            last_name=self.last_name
+        ).strip()
