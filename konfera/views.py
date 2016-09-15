e3e5338fc4d8526b3d056834b46d379538ac1268
Tomáš Horváth <tomas.horvath@gmail.com>
2016-09-15 19:12:17 +0200

    sponsor_list_view added, basic templates created

--- konfera/views.py
+++ konfera/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from konfera.models import Event, Sponsor
 
-# Create your views here.
+
+def sponsor_list_view(request):
+    events = Event.objects.all()
+    sponsors = Sponsor.objects.all()
+    context = {'events': events,
+               'sponsors': sponsors,
+               }
+
+    return render(request=request,
+                  template_name='konfera/sponsors_per_event.html',
+                  context=context)
