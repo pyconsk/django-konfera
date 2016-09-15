c0a0262bd8bfba3f61b78e03d7ee42d4d99439db
Tomáš Horváth <tomas.horvath@gmail.com>
2016-09-15 23:30:45 +0200

    Add urls.py file rename template sponsors_per_event.html => event_sponsors.html base.html refactor refactor sponsor_list_view using select_related()

--- konfera/views.py
+++ konfera/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from konfera.models import Event, Sponsor
 
 
-def sponsor_list_view(request):
-    events = Event.objects.all()
-    sponsors = Sponsor.objects.all()
-    context = {'events': events,
+def sponsor_list_view(request, event_id):
+    sponsors = Sponsor.objects.select_related('event') \
+                    .filter(event__id=event_id).order_by('type')
+    event = Event.objects.get(pk=event_id)
+    context = {'event': event,
                'sponsors': sponsors,
                }
 
     return render(request=request,
-                  template_name='konfera/sponsors_per_event.html',
+                  template_name='konfera/event_sponsors.html',
                   context=context)
