1fac5310d24a397f4fa2fac9e70b674905b99569
Tomáš Horváth <tomas.horvath@gmail.com>
2016-09-16 18:21:55 +0200

    PEP8  update

--- konfera/views.py
+++ konfera/views.py
@@ -5,7 +5,7 @@ from konfera.models import Event, Sponsor
 def sponsor_list_view(request, event_slug):
     event = get_object_or_404(Event, slug=event_slug)
     sponsors = Sponsor.objects.select_related('event') \
-                    .filter(event=event).order_by('type')
+        .filter(event=event).order_by('type')
 
     context = {'event': event,
                'sponsors': sponsors,
