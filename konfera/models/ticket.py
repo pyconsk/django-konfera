b8014eb3b3577310c826a1eefea1c12bab1f4d0b
Richard Kellner <richard@kellnerovci.sk>
2016-09-16 13:38:59 +0200

    Fixed failing test

--- konfera/models/ticket.py
+++ konfera/models/ticket.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from konfera.models.speaker import TITLE_UNSET, TITLE_CHOICES
+
 
 TICKET_STATUS = (
     ('requested', 'Requested'),
@@ -13,6 +15,7 @@ class Ticket(models.Model):
     type = models.ForeignKey('TicketType')
     discount_code = models.ForeignKey('DiscountCodes')
     status = models.CharField(choices=TICKET_STATUS, max_length=32)
+    title = models.CharField(choices=TITLE_CHOICES, max_length=4, default=TITLE_UNSET)
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
     email = models.EmailField()
@@ -21,4 +24,8 @@ class Ticket(models.Model):
     order = models.ForeignKey('Order')
 
     def __str__(self):
-        return "Ticket of Mr/Mrs/Ms {}".format(self.last_name)
+        return '{title} {first_name} {last_name}'.format(
+            title=dict(TITLE_CHOICES)[self.title],
+            first_name=self.first_name,
+            last_name=self.last_name
+        ).strip()
