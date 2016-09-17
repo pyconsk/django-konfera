18de955dcccf8f3ec77c22a015c422b58ff0d0fb
Richard Rožár <Redpoints1@gmail.com>
2016-09-17 13:57:19 +0200

    Defined roots/urls for static & media files (#61)

--- example/example/urls.py
+++ example/example/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('konfera.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
