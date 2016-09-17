f2241f6bf8ada0586ff6c239be6e876ed6e8a5a7
Richard Kellner <richard@kellnerovci.sk>
2016-09-17 18:16:07 +0200

    Updated event admin view, dates validation, and tests for new validation

--- konfera/admin.py
+++ konfera/admin.py
@@ -1,18 +1,44 @@
 from django.contrib import admin
 
-from konfera.models import (Receipt, Order, Location, Event, Sponsor,
-                            TicketType, DiscountCodes, Ticket, Speaker,
-                            Talk, Room, Schedule)
+from konfera.models import (Receipt, Order, Location, Event, Sponsor, TicketType, DiscountCodes, Ticket, Speaker, Talk,
+                            Room, Schedule)
+
+
+class SponsorshipInline(admin.TabularInline):
+    model = Event.sponsors.through
+    extra = 1
 
 
 class EventAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
+    list_display = ('title', 'date_from', 'date_to', 'event_type', 'status')
+    list_filter = ('event_type', 'status')
+
+    fieldsets = (
+        ('Description', {
+            'fields': ('title', 'slug', 'description'),
+        }),
+        ('Dates', {
+            'fields': ('date_from', 'date_to'),
+        }),
+        ('Details', {
+            'fields': ('event_type', 'status', 'location'),
+        }),
+    )
+
+    inlines = [
+        SponsorshipInline,
+    ]
+
+    prepopulated_fields = {
+        'slug': ('title',),
+    }
+
+admin.site.register(Event, EventAdmin)
 
 
 admin.site.register(Receipt)
 admin.site.register(Order)
 admin.site.register(Location)
-admin.site.register(Event, EventAdmin)
 admin.site.register(Sponsor)
 admin.site.register(TicketType)
 admin.site.register(DiscountCodes)
--- konfera/models/event.py
+++ konfera/models/event.py
@@ -1,23 +1,26 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+
 
 EVENT_TYPE_CHOICES = (
-    ('conference', 'Conference'),
-    ('meetup', 'Meetup'),
+    ('conference', _('Conference')),
+    ('meetup', _('Meetup')),
 )
 
 EVENT_STATUS_CHOICES = (
-    ('draft', 'Draft'),
-    ('published', 'Published'),
-    ('expired', 'Expired'),
+    ('draft', _('Draft')),
+    ('published', _('Published')),
+    ('expired', _('Expired')),
 )
 
 
 class Event(models.Model):
-    title = models.CharField(max_length=128)
-    slug = models.SlugField()
+    title = models.CharField(verbose_name=_('Event name'), max_length=128)
+    slug = models.SlugField(verbose_name=_('Event url'), help_text=_('Slug field, relative URL to the event.'))
     description = models.TextField()
-    date_from = models.DateTimeField()
-    date_to = models.DateTimeField()
+    date_from = models.DateTimeField(verbose_name=_('Event begging'))
+    date_to = models.DateTimeField(verbose_name=_('Event end'))
     event_type = models.CharField(choices=EVENT_TYPE_CHOICES, max_length=20)
     status = models.CharField(choices=EVENT_STATUS_CHOICES, max_length=20)
     location = models.ForeignKey('Location')
@@ -25,3 +28,10 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+
+    def clean(self):
+        if self.date_from > self.date_to:
+            raise ValidationError(_('%(date_from)s have to be before %(date_to)s'),
+                                  params={'date_from': self._meta.get_field('date_from').verbose_name,
+                                          'date_to': self._meta.get_field('date_to').verbose_name},
+                                  )
