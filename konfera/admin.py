1def2718c44eb4044d1fc09d7e05629b878aec64
Redpoint1 <Redpoints1@gmail.com>
2016-09-17 22:44:28 +0200

    Improved administration for Speaker, see #68

--- konfera/admin.py
+++ konfera/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from konfera.models import (Receipt, Order, Location, Event, Sponsor,
                             TicketType, DiscountCode, Ticket, Speaker,
@@ -9,6 +10,24 @@ class EventAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
+class SpeakerAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'country', 'social_url',)
+    list_filter = ('country', 'title', 'sponsor',)
+    ordering = ('last_name', 'first_name',)
+    search_fields = ('=last_name', '=first_name',)  # case insensitive searching
+    fieldsets = (
+        (_('Name'), {
+            'fields': ('title', ('first_name', 'last_name',),)
+        }),
+        (_('Contact'), {
+            'fields': ('email', 'phone',)
+        }),
+        (_('About'), {
+            'fields': ('bio', 'country', ('url', 'social_url',), 'sponsor',)
+        }),
+    )
+
+
 admin.site.register(Receipt)
 admin.site.register(Order)
 admin.site.register(Location)
@@ -17,7 +36,7 @@ admin.site.register(Sponsor)
 admin.site.register(TicketType)
 admin.site.register(DiscountCode)
 admin.site.register(Ticket)
-admin.site.register(Speaker)
+admin.site.register(Speaker, SpeakerAdmin)
 admin.site.register(Talk)
 admin.site.register(Room)
 admin.site.register(Schedule)
