a720f9e4c90222460364824fd9eb89a2a26ac670
Richard Kellner <richard.kellner@erigones.com>
2016-09-22 14:31:32 +0200

    53 minor model issues (#86)
    * Added basic bootstrap to base template due to issue #21
    * Added abstract FromToModel that add from to fields and does the validation in issue #53
    * Updated event admin view, dates validation, and tests for new validation
    * Implement LocationAdmin
    * Updated validation in talk speakers due to issue #53
    * Added schedule validation due to issue #53
    * Improoved talk fields descriptions
    * Fixed tests due to issue #53 (Suppressed timezone warnings)
    * Added test for ticket type to test the dates in issue #53
    * Added tests that speakers doesnt match and schedule date range in issue #53

--- konfera/models/schedule.py
+++ konfera/models/schedule.py
@@ -1,12 +1,22 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Schedule(models.Model):
     start = models.DateTimeField()
-    description = models.TextField()
-    talk = models.ForeignKey('Talk')
-    duration = models.IntegerField()
-    room = models.ForeignKey('Room')
+    description = models.TextField(
+        blank=True,
+        help_text=_('Description will be displayed, only if there is no related talk, eg. coffee break, lunch etc...'))
+    talk = models.ForeignKey('Talk', blank=True, related_name='scheduled_talks')
+    duration = models.IntegerField(
+        default=0,
+        validators=[
+            MaxValueValidator(300),
+            MinValueValidator(0)
+        ],
+        help_text=_('Duration in minutes.'))
+    room = models.ForeignKey('Room', blank=True, related_name='scheduled_rooms')
 
     def __str__(self):
         return '%s (%s min)' % (self.start, self.duration)
--- konfera/models/talk.py
+++ konfera/models/talk.py
@@ -1,5 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.db import models
-
+from django.utils.translation import ugettext_lazy as _
 
 TALK_STATUS = (
     ('cfp', 'Call For Proposals'),
@@ -23,13 +24,33 @@ TALK_DURATION = (
 
 class Talk(models.Model):
     title = models.CharField(max_length=256)
-    abstract = models.TextField()
+    abstract = models.TextField(help_text=_('Abstract will be published in the schedule.'))
     type = models.CharField(choices=TALK_TYPE, max_length=32)
     status = models.CharField(choices=TALK_STATUS, max_length=32)
-    duration = models.IntegerField(choices=TALK_DURATION)
-    primary_speaker = models.ForeignKey('Speaker', related_name='secondary_speaker')
-    secondary_speaker = models.ForeignKey('Speaker', related_name='primary_speaker', blank=True, null=True)
+    duration = models.IntegerField(choices=TALK_DURATION, help_text=_('Talk duration in minutes.'))
+    primary_speaker = models.ForeignKey(
+        'Speaker',
+        related_name='primary_speaker_talks',
+        verbose_name=_('Primary speaker'),
+    )
+    secondary_speaker = models.ForeignKey(
+        'Speaker',
+        related_name='secondary_speaker_talks',
+        verbose_name=_('Secondary speaker'),
+        blank=True,
+        null=True,
+    )
     event = models.ForeignKey('Event')
 
     def __str__(self):
         return self.title
+
+    def clean(self):
+        if hasattr(self, 'primary_speaker') and hasattr(self, 'secondary_speaker') \
+                and self.primary_speaker == self.secondary_speaker:
+            msg = _('%(primary)s have to be different than %(secondary)s.') % {
+                'primary': self._meta.get_field('primary_speaker').verbose_name,
+                'secondary': self._meta.get_field('secondary_speaker').verbose_name
+            }
+
+            raise ValidationError({'primary_speaker': msg, 'secondary_speaker': msg})
