ebaa579e54b967d3dfe2a13bc2614d60720a4343
Tomáš Horváth <tomas.horvath@gmail.com>
2016-09-23 12:24:35 +0200

    Minor Model changes and new migration

--- konfera/models/talk.py
+++ konfera/models/talk.py
@@ -1,8 +1,11 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
+CFP = 'cfp'
 
 TALK_STATUS = (
-    ('cfp', 'Call For Proposals'),
+    (CFP, 'Call For Proposals'),
     ('draft', 'Draft'),
     ('approved', 'Approved'),
     ('rejected', 'Rejected'),
@@ -23,13 +26,33 @@ TALK_DURATION = (
 
 class Talk(models.Model):
     title = models.CharField(max_length=256)
-    abstract = models.TextField()
-    type = models.CharField(choices=TALK_TYPE, max_length=32)
+    abstract = models.TextField(help_text=_('Abstract will be published in the schedule.'))
+    type = models.CharField(choices=TALK_TYPE, max_length=32, default='talk')
     status = models.CharField(choices=TALK_STATUS, max_length=32)
-    duration = models.IntegerField(choices=TALK_DURATION)
-    primary_speaker = models.ForeignKey('Speaker', related_name='secondary_speaker')
-    secondary_speaker = models.ForeignKey('Speaker', related_name='primary_speaker', blank=True, null=True)
+    duration = models.IntegerField(choices=TALK_DURATION, help_text=_('Talk duration in minutes.'), default=30)
+    primary_speaker = models.ForeignKey(
+        'Speaker',
+        related_name='primary_speaker_talks',
+        verbose_name=_('Primary speaker'),
+    )
+    secondary_speaker = models.ForeignKey(
+        'Speaker',
+        related_name='secondary_speaker_talks',
+        verbose_name=_('Secondary speaker'),
+        blank=True,
+        null=True,
+    )
     event = models.ForeignKey('Event')
 
     def __str__(self):
         return self.title
+
+    def clean(self):
+        if hasattr(self, 'primary_speaker') and hasattr(self, 'secondary_speaker') \
+                and self.primary_speaker == self.secondary_speaker:
+            msg = _('%(primary)s have to be different than %(secondary)s.') % {
+                'primary': self._meta.get_field('primary_speaker').verbose_name,
+                'secondary': self._meta.get_field('secondary_speaker').verbose_name
+            }
+
+            raise ValidationError({'primary_speaker': msg, 'secondary_speaker': msg})
