615cc5d9d11e9a85810cae34fb31b8a07ade45da
Krystof Kiss <krystof.kiss@gmail.com>
2016-09-24 00:41:19 +0200

    create and register TicketTypeAdmin

--- konfera/admin.py
+++ konfera/admin.py
@@ -129,8 +129,22 @@ class OrderAdmin(admin.ModelAdmin):
 admin.site.register(Order, OrderAdmin)
 
 
+class TicketTypeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'attendee_type', 'event',)
+    list_filter = ('attendee_type',)
+    fieldsets = (
+        (_('Details'), {
+            'fields': ('title', 'description', 'price', 'attendee_type', 'event',)
+        }),
+        (_('Availability'), {
+            'fields': ('date_from', 'date_to',)
+        }),
+    )
+
+admin.site.register(TicketType, TicketTypeAdmin)
+
+
 admin.site.register(Receipt)
-admin.site.register(TicketType)
 admin.site.register(DiscountCode)
 admin.site.register(Ticket)
 admin.site.register(Room)
