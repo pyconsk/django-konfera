d4d6ea5944c04716a6bffef33be44859d126cb23
Tomas <TomasHo@users.noreply.github.com>
2016-09-26 17:02:45 +0200

    #20-cfp-view (#87)
    * add views, forms, urls and template for adding speaker and/or talk
    
    * removed messages, refactor cfp_form.html
    refactor forms.py
    refactor cfp_form_view in views.py
    refactor urls.py
    
    * cfp_form title removed
    django messages added to base.html
    view update, status set to 'cfp' by default, message refactor
    remove Secondary speaker field from form
    
    * add translation support to cfp_form.html
    minor changes to forms
    update urls to per event cfp_form
    update cfp_form_view to support translation
    minor changes to variable handling
    
    * move translation to template
    
    * pep8 changes

--- konfera/views.py
+++ konfera/views.py
@@ -1,7 +1,12 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.utils.translation import ugettext_lazy as _
 
-from konfera.models import Event, Talk
+from konfera.models import Event
+from konfera.models.talk import CFP
+from konfera.forms import SpeakerForm, TalkForm
 
 
 def event_sponsors_list_view(request, event_slug):
@@ -59,4 +64,26 @@ def event_details_view(request, event_slug):
 
     return render(request=request,
                   template_name='konfera/event_details.html',
+                  context=context, )
+
+
+def cfp_form_view(request, event_slug):
+    speaker_form = SpeakerForm(request.POST or None, prefix='speaker')
+    talk_form = TalkForm(request.POST or None, prefix='talk')
+    if speaker_form.is_valid() and talk_form.is_valid():
+        speaker_instance = speaker_form.save()
+        talk_instance = talk_form.save(commit=False)
+        talk_instance.primary_speaker = speaker_instance
+        talk_instance.status = CFP
+        talk_instance.event = Event.objects.get(slug=event_slug)
+        talk_instance.save()
+        message_text = _("Your talk proposal successfully created")
+        messages.success(request, message_text)
+        return HttpResponseRedirect(redirect_to='/event/')
+    context = {'speaker_form': speaker_form,
+               'talk_form': talk_form,
+               }
+
+    return render(request=request,
+                  template_name='konfera/cfp_form.html',
                   context=context, )
