5f79633178761206e4489997b8b1c6977f4c002b
Zoltan Onody <zoltan.onody@gmail.com>
2016-10-02 00:03:57 +0200

    Set default values for discount & amount_paid in Order

--- konfera/models/order.py
+++ konfera/models/order.py
@@ -23,8 +23,8 @@ ORDER_CHOICES = (
 
 class Order(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=12, validators=[MinValueValidator(0)])
-    amount_paid = models.DecimalField(decimal_places=2, max_digits=12, validators=[MinValueValidator(0)])
-    discount = models.DecimalField(decimal_places=2, max_digits=12, validators=[MinValueValidator(0)])
+    amount_paid = models.DecimalField(decimal_places=2, max_digits=12, validators=[MinValueValidator(0)], default=0)
+    discount = models.DecimalField(decimal_places=2, max_digits=12, validators=[MinValueValidator(0)], default=0)
     status = models.CharField(choices=ORDER_CHOICES, default=AWAITING, max_length=20)
     purchase_date = models.DateTimeField(auto_now_add=True)
     payment_date = models.DateTimeField(blank=True, null=True)
