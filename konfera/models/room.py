b803ca9b1f5cfeca316eed6c20c1ca037c6940ac
Zoltan Onody <zoltan.onody@gmail.com>
2016-10-01 22:41:42 +0200

    IngeterField must have null=True if blank=True

--- konfera/models/room.py
+++ konfera/models/room.py
@@ -4,7 +4,7 @@ from django.db import models
 class Room(models.Model):
     title = models.CharField(max_length=128)
     location = models.ForeignKey('Location', related_name='rooms')
-    capacity = models.IntegerField(blank=True)
+    capacity = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.title
