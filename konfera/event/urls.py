0c02f13e31697beb5d5de86b2c98977e32c50d20
Zoltan Onody <zoltan.onody@gmail.com>
2016-10-04 18:41:42 +0200

    Refactor events

---
+++ konfera/event/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from konfera.event import views
+
+
+urlpatterns = [
+    url(r'^(?P<event_slug>[\w, -]+)/speakers/$', views.event_speakers_list_view,
+        name='event_speakers'),
+    url(r'^(?P<event_slug>[\w, -]+)/sponsors/$', views.event_sponsors_list_view,
+        name='event_sponsors'),
+    url(r'^(?P<event_slug>[\w, -]+)/details/$', views.event_details_view,
+        name='event_details'),
+    url(r'^(?P<event_slug>[\w, -]+)/add_cfp/$', views.cfp_form_view, name='event_cfp_form'),
+    url(r'^$', views.event_list, name='events'),
+]
