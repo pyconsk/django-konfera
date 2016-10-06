3912d173169d0df4f52b53c8ee16d3ed82dd160a
Zoltan Onody <zoltan.onody@gmail.com>
2016-10-06 21:17:14 +0200

    Add schedule view

--- konfera/event/urls.py
+++ konfera/event/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     url(r'^(?P<event_slug>[\w, -]+)/details/$', views.event_details_view,
         name='event_details'),
     url(r'^(?P<event_slug>[\w, -]+)/add_cfp/$', views.cfp_form_view, name='event_cfp_form'),
+    url(r'^(?P<slug>[\w, -]+)/schedule/$', views.schedule_redirect, name='schedule'),
+    url(r'^(?P<slug>[\w, -]+)/schedule/(?P<day>[\d, -]+)$', views.ScheduleView.as_view(), name='schedule'),
     url(r'^$', views.event_list, name='events'),
 ]
