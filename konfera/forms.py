ca2ecc33c0e7ac4b29ecd1504d99b15e606d2e5e
kapucko <dkapisinsky@gmail.com>
2016-10-08 01:31:45 +0200

    not used anymore

--- konfera/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from konfera.models import Ticket
-
-
-class VolunteerRegistrationForm(forms.ModelForm):
-    class Meta:
-        model = Ticket
-        fields = ('title', 'first_name', 'last_name', 'email', 'phone', 'description')
