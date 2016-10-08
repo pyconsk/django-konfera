258cf455b2c2707d6b5b44983d5df0d2f90104ab
Dominik Kapisinsky <dkapisinsky@gmail.com>
2016-10-08 09:13:30 +0200

    #125 register module (#146)
    * #125 moved register stuff code to register submodule

--- konfera/register/views.py
+++ konfera/register/views.py
@@ -1,15 +1,8 @@
-from django.contrib import messages
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
-from django.utils.translation import ugettext_lazy as _
-
-from konfera.event.forms import SpeakerForm, TalkForm
-from konfera.forms import VolunteerRegistrationForm
-from konfera.models.event import Event
-from konfera.models.talk import CFP
+from konfera.models import Event, TicketType
+from konfera.models.ticket_type import VOLUNTEER
+from konfera.register.forms import VolunteerRegistrationForm
 from konfera.models.ticket import REQUESTED
-from konfera.models.ticket_type import TicketType, VOLUNTEER
 
 
 def register_volunteer(request, event_slug):
