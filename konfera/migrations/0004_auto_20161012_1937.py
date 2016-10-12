ee90f92209e3d5591bf2c813ff0584c48fa896fc
Sourabh.Suman <sourabh.su@media.net>
2016-10-13 01:15:05 +0530

    changing dependencies

--- konfera/migrations/0004_auto_20161012_1937.py
+++ konfera/migrations/0004_auto_20161012_1937.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('konfera', '0003_auto_20161011_1907'),
+        ('konfera', '0002_schedule_event.py'),
     ]
 
     operations = [
