a6c946549ba01b9aeb01dc18056c97acfca46492
Issa Jubril <issa.jubril@andela.com>
2016-10-17 20:29:04 +0100

    solved Issue #120 (#164)
    * Make description field required and add placeholders
    * Set ticket type to public after volunteer registers
    * Show successful message after storing in database
    * Code refactor
    * fix flake8 issue
    * Fix indentation issue

--- konfera/register/forms.py
+++ konfera/register/forms.py
@@ -7,3 +7,11 @@ class VolunteerRegistrationForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ('title', 'first_name', 'last_name', 'email', 'phone', 'description')
+
+    def __init__(self, *args, **kwargs):
+        super(VolunteerRegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['description'].widget.attrs\
+            .update({
+                'required': 'required',
+                'placeholder': 'Please tell us something about yourself...'
+            })
