c2ca7f33ef10cd64b03c60a6fb941364eb7ca45c
Jubril Issa <issa.jubril@andela.com>
2016-10-17 15:29:52 +0100

    Code refactor

--- konfera/register/views.py
+++ konfera/register/views.py
@@ -1,31 +1,30 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib import messages
 
 from konfera.models.event import Event, MEETUP
 from konfera.models.ticket import REQUESTED
-from konfera.models.ticket_type import TicketType, VOLUNTEER
+from konfera.models.ticket_type import TicketType, VOLUNTEER, PUBLIC
 from konfera.register.forms import VolunteerRegistrationForm
 
 
 def register_volunteer(request, slug):
     context = dict()
     event = get_object_or_404(Event, slug=slug)
-    volunteer_ticket_type = get_object_or_404(TicketType, event=event.id, attendee_type=VOLUNTEER)
+    volunteer_ticket_type = get_object_or_404(TicketType, event=event.id, attendee_type=VOLUNTEER, accessibility=PUBLIC)
 
-    if request.method == "POST":
-        form = VolunteerRegistrationForm(request.POST)
+    form = VolunteerRegistrationForm(request.POST or None)
 
-        if form.is_valid():
-            new_ticket = form.save(commit=False)
-            new_ticket.status = REQUESTED
-            new_ticket.type = volunteer_ticket_type
-            new_ticket.save()
+    if form.is_valid():
+        new_ticket = form.save(commit=False)
+        new_ticket.status = REQUESTED
+        new_ticket.type = volunteer_ticket_type
+        new_ticket.save()
 
-            if volunteer_ticket_type.accessibility != 'public':
-                volunteer_ticket_type.accessibility = 'public'
-                volunteer_ticket_type.save()
+        message_text = ("Thanks for registering...")
+        messages.success(request, message_text)
 
-            return redirect('event_details', slug)
+        return redirect('event_details', slug)
     else:
         form = VolunteerRegistrationForm()
 
--- konfera/static/konfera/js/pycon.js
+++
@@ -1,8 +0,0 @@
-$(document).ready (function(){
-  // Close the volunteer registration alert 2 seconds after it is displayed
-  $("#success-alert").alert();
-  window.setTimeout(function() {
-    $("#success-alert").alert('close');
-  }, 2000);
-
- });
