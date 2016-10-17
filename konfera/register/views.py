21dada0a11d804a2f040964896868156aaa211af
Neela Balkaran <neelabalkaran@hotmail.com>
2016-10-17 15:05:16 -0400

    Initial commit for meetup changes (#163)
    * Initial commit for meetup changes
    * Fix meetup URL
    * Fix meetup page styling

--- konfera/register/views.py
+++ konfera/register/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from konfera.models.event import Event, MEETUP
+from konfera.models.event import Event
 from konfera.models.ticket import REQUESTED
 from konfera.models.ticket_type import TicketType, VOLUNTEER
 from konfera.register.forms import VolunteerRegistrationForm
@@ -29,25 +29,3 @@ def register_volunteer(request, slug):
     context['type'] = VOLUNTEER
 
     return render(request, 'konfera/registration_form.html', context=context)
-
-
-def meetup_list(request):
-    meetups = Event.objects.filter(event_type=MEETUP).order_by('date_from')
-    context = dict()
-
-    if meetups.count() == 1:
-        return redirect('meetup_detail', event_slug=meetups[0].slug)
-
-    paginator = Paginator(meetups, 5)
-    page = request.GET.get('page')
-
-    try:
-        meetups = paginator.page(page)
-    except PageNotAnInteger:
-        meetups = paginator.page(1)
-    except EmptyPage:
-        meetups = paginator.page(paginator.num_pages)
-
-    context['meetups'] = meetups
-
-    return render(request, 'konfera/meetups.html', context=context)
