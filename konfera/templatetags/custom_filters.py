41d7900337ddefcfd4de09245d5a173b4252b19f
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2016-11-03 12:00:50 -0300

    feat: Use settings language as default
    Now set_default_locale uses settings LANGUAGE_CODE as default locale.
    Also add tests to unsuported locales, but theses locales depend on the
    system running the application.

--- konfera/templatetags/custom_filters.py
+++ konfera/templatetags/custom_filters.py
@@ -1,11 +1,19 @@
 from django import template
+from django.utils.translation import get_language, to_locale
+from django.utils.encoding import get_system_encoding
 import locale
 
 register = template.Library()
 
 
 def set_default_locale():
-    user_locale = locale.setlocale(locale.LC_ALL, ('en_US', 'UTF-8'))
+    default_language = get_language()
+    default_loc = to_locale(default_language)
+    system_enc = get_system_encoding()
+    try:
+        user_locale = locale.setlocale(locale.LC_ALL, (default_loc, system_enc))
+    except locale.Error:
+        user_locale = locale.setlocale(locale.LC_ALL, ('en_US', 'UTF-8'))
     return user_locale
 
 
--- konfera/tests/test_templatetags.py
+++ konfera/tests/test_templatetags.py
@@ -27,5 +27,18 @@ class TestCurrencyTag(TestCase):
 
 
 class TestSetUserLocale(TestCase):
+    @override('en-us')
     def test_locale_is_en_US(self):
         self.assertEqual(set_default_locale(), 'en_US.UTF-8')
+
+    @override('pt-br')
+    def test_unsupported_locale1(self):
+        self.assertIn(set_default_locale(), ['pt_BR.UTF-8', 'en_US.UTF-8'])
+
+    @override('zh-cn')
+    def test_unsupported_locale2(self):
+        self.assertIn(set_default_locale(), ['zh_CN.UTF-8', 'en_US.UTF-8'])
+
+    @override('ro-mo')
+    def test_unsupported_locale3(self):
+        self.assertIn(set_default_locale(), ['ro_MO.UTF-8', 'en_US.UTF-8'])
