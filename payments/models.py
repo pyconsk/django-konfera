823212db203f42b26ec538f4255b333825913c9b
Zoltan Onody <zoli@ksp.sk>
2016-11-27 23:54:50 +0100

    Increase transaction_id len to 64 (#236)

--- payments/models.py
+++ payments/models.py
@@ -3,7 +3,7 @@ from konfera.models.abstract import KonferaModel
 
 
 class ProcessedTransaction(KonferaModel):
-    transaction_id = models.CharField(max_length=20, unique=True)
+    transaction_id = models.CharField(max_length=64, unique=True)
     variable_symbol = models.CharField(max_length=10)
     amount = models.DecimalField(decimal_places=2, max_digits=10)
     date = models.DateField(blank=True, null=True)
