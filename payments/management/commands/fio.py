8d6f0f9606136f227a66bc17951ecfae0fad1e14
Richard Kellner <richard.kellner@erigones.com>
2016-11-28 23:44:04 +0100

    Updated fio bank management command (#242)
    * Updated fio bank management command
    * Fixed test to generate the dates instead of constant

--- payments/management/commands/fio.py
+++ payments/management/commands/fio.py
@@ -1,11 +1,18 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 from payments.utils import check_payments_status
+
+from fiobank import ThrottlingError
 
 
 class Command(BaseCommand):
     help = 'Process payments in FIO Bank.'
 
     def handle(self, *args, **options):
-        check_payments_status()
-        self.stdout.write(self.style.SUCCESS('Successfully processed awaiting and partly paid orders'))
+        try:
+            check_payments_status(verbose=options['verbosity'])
+        except ThrottlingError:
+            raise CommandError('Command should be used only once per 30s.')
+
+        if options['verbosity'] > 0:
+            self.stdout.write(self.style.SUCCESS('Successfully processed awaiting and partly paid orders'))
