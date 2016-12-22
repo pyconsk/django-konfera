edce543ed7c15dbd1ee740a229b92cacb7d287ac
Bezrucka <juraj.bezrucka@gmail.com>
2016-12-22 12:27:33 +0100

    #256 email util: validation test

---
+++ konfera/tests/test_utils.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+from konfera.utils import validate_email_template
+
+class EmailUtilTest(TestCase):
+
+    def test_validate_email_template(self):
+        template = 'Hello {name}'
+        valid_input = {'name': 'Joe'}
+        invalid_input = {'Joe': 'name'}
+        self.assertEqual(validate_email_template(template, valid_input), 'Hello Joe')
+        self.assertEqual(validate_email_template(template, invalid_input), None)
