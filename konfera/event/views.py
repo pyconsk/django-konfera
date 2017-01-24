1464d74ee53e5f3f26d7641ea6daf41fc8f2e02f
Richard Kellner <richard.kellner@pycon.sk>
2017-01-24 21:39:48 +0100

    Updated speakers list to display only unique speakers

--- konfera/event/views.py
+++ konfera/event/views.py
@@ -61,7 +61,7 @@ def event_speakers_list_view(request, slug):
     context['speakers'] = Speaker.objects.filter(
         (Q(primary_speaker_talks__event__slug=slug) | Q(secondary_speaker_talks__event__slug=slug)) &
         (Q(primary_speaker_talks__status=Talk.APPROVED) | Q(secondary_speaker_talks__status=Talk.APPROVED))
-    ).order_by('last_name')
+    ).order_by('last_name').distinct()
 
     update_event_context(event, context)
 
