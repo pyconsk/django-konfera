c7e5591c3371340e21629a5d27fe501ac6bc4cb6
Richard Kellner <richard.kellner@pycon.sk>
2017-01-29 20:17:42 +0100

    Added test if title has been filled in, so admin does not throw exception

--- konfera/models/ticket.py
+++ konfera/models/ticket.py
@@ -33,11 +33,19 @@ class Ticket(KonferaModel):
     order = models.ForeignKey('Order', blank=True)
 
     def __str__(self):
-        return '{title} {first_name} {last_name}'.format(
-            title=dict(Speaker.TITLE_CHOICES)[self.title],
-            first_name=self.first_name,
-            last_name=self.last_name
-        ).strip()
+        if self.title:
+            full_name = '{title} {first_name} {last_name}'.format(
+                title=dict(Speaker.TITLE_CHOICES)[self.title],
+                first_name=self.first_name,
+                last_name=self.last_name
+            )
+        else:
+            full_name = '{first_name} {last_name}'.format(
+                first_name=self.first_name,
+                last_name=self.last_name
+            )
+
+        return full_name.strip()
 
     def discount_calculator(self):
         if self.discount_code:
