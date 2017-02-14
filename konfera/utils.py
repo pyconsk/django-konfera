e72b0ed091a4c62a3d492f5cbcfbea6be48612de
Richard Kellner <richard.kellner@erigones.com>
2017-02-14 17:31:00 +0100

    Improoved sponsors page design (#301)
    * Updated sponsors view
    * Fixed issue when right hand side sponsors section didnt show sponsors
    * Removed Medial partners for the main page
    * Fixed logo styling for sponsors with greater height than width...

--- konfera/utils.py
+++ konfera/utils.py
@@ -45,8 +45,8 @@ def update_event_context(event, context, show_sponsors=True):
     context['event'] = event
 
     if show_sponsors:
-        frontend_sponsors = (Sponsor.PLATINUM, Sponsor.GOLD, Sponsor.SILVER, Sponsor.MEDIA)
-        context['sponsors'] = event.sponsors.filter(type__in=frontend_sponsors).order_by('type', 'title')
+        frontend_sponsors = (Sponsor.PLATINUM, Sponsor.GOLD, Sponsor.SILVER)
+        context['show_sponsors'] = event.sponsors.filter(type__in=frontend_sponsors).order_by('type', 'title')
 
     if event.analytics:
         context['ga'] = event.analytics
