13a517f8dbacd50bfb6f6ee4aae392cc1e5f23e7
Richard Kellner <richard.kellner@gmail.com>
2017-09-21 11:31:11 +0200

    Added flag for event to be able to add grouping for talks (#327)
    * Added flag for event to be able to add grouping for talks
    
    Once we add a same flag for certian talks we can mark them as track,
    base filters etc...
    
    * Updated model atribute name made in error in previous commit

--- konfera/models/talk.py
+++ konfera/models/talk.py
@@ -51,6 +51,7 @@ class Talk(KonferaModel):
         blank=True,
         null=True,
     )
+    flag = models.CharField(max_length=32, blank=True)
     event = models.ForeignKey('Event')
 
     def __str__(self):
